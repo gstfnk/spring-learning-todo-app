b54cd589de4a4fddbebf6bb2023288a094391987
gstfnk <gabrysia.stefaniak@gmail.com>
2021-08-10 13:15:34 +0200

    fix id

--- src/main/java/io/github/gstfnk/model/Task.java
+++ src/main/java/io/github/gstfnk/model/Task.java
@@ -1,17 +1,19 @@
 package io.github.gstfnk.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tasks")
 class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String description;
     private boolean done;
+
+    public Task() {
+    }
 
     public int getId() {
         return id;
