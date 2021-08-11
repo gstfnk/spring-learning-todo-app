2ff2c2e192a7a8cb1c7a7e87f2e8227b4e10db92
gstfnk <gabrysia.stefaniak@gmail.com>
2021-08-11 20:50:15 +0200

    improve TaskRepo

--- src/main/java/io/github/gstfnk/model/TaskRepository.java
+++ src/main/java/io/github/gstfnk/model/TaskRepository.java
@@ -1,23 +1,25 @@
 package io.github.gstfnk.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
+import java.util.Optional;
 
-@RepositoryRestResource
-public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
+public interface TaskRepository {
+    List<Task> findAll();
 
-    @Override
-    @RestResource(exported = false)
-    void delete(Task task);
+    Page<Task> findAll(Pageable page);
 
-    @RestResource(path = "done", rel = "done")
+    Optional<Task> findById(Integer id);
+
+    boolean existsById(Integer id);
+
     List<Task> findByDone(@Param("state") boolean done);
+
+    Task save(Task entity);
+
+    void deleteById(Integer id);
 }
