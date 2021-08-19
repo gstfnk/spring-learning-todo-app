268f8a807efdf77ba3ed7d0ba9a45ec61c169c3c
gstfnk <gabrysia.stefaniak@gmail.com>
2021-08-19 16:24:58 +0200

    fix V1 migration and add V2

--- src/main/resources/db/migration/V1__init_tasks_table.sql
+++ src/main/resources/db/migration/V1__init_tasks_table.sql
@@ -1,5 +1,5 @@
-drop table if exists TASKS;
-create table TASKS(
+drop table if exists tasks;
+create table tasks(
     id int primary key auto_increment,
     description varchar(100) not null,
     done bit
