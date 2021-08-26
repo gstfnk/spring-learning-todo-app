ce80e3e5c410bc3b96e90dbc710840bf1ca25fc7
gstfnk <gabrysia.stefaniak@gmail.com>
2021-08-26 22:42:56 +0200

    add Project model

---
+++ src/main/java/io/github/gstfnk/adapter/SqlProjectRepository.java
@@ -0,0 +1,19 @@
+package io.github.gstfnk.adapter;
+
+import io.github.gstfnk.model.TaskGroup;
+import io.github.gstfnk.model.TaskGroupRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
+
+    @Override
+    boolean existsByDoneIsFalseAndProject_Id(Integer projectId);
+}
---
+++ src/main/java/io/github/gstfnk/model/Project.java
@@ -0,0 +1,53 @@
+package io.github.gstfnk.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "PROJECTS")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Project's description must be not empty")
+    private String description;
+    @OneToMany(mappedBy = "project")
+    private Set<TaskGroup> groups;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
+    private Set<ProjectStep> steps;
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    Set<TaskGroup> getGroups() {
+        return groups;
+    }
+
+    void setGroups(Set<TaskGroup> groups) {
+        this.groups = groups;
+    }
+
+    Set<ProjectStep> getSteps() {
+        return steps;
+    }
+
+    void setSteps(Set<ProjectStep> steps) {
+        this.steps = steps;
+    }
+}
