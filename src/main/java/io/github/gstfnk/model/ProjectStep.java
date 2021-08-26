7c695cafad4f47275d6759662b84e6115df28c7a
gstfnk <gabrysia.stefaniak@gmail.com>
2021-08-26 22:44:30 +0200

    add project_steps entity

---
+++ src/main/java/io/github/gstfnk/model/ProjectStep.java
@@ -0,0 +1,53 @@
+package io.github.gstfnk.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "PROJECT_STEPS")
+public class ProjectStep {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Project step's description must be not empty")
+    private String description;
+    private int daysToDeadline;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDaysToDeadline() {
+        return daysToDeadline;
+    }
+
+    void setDaysToDeadline(int daysToDeadline) {
+        this.daysToDeadline = daysToDeadline;
+    }
+
+    Project getProject() {
+        return project;
+    }
+
+    void setProject(Project project) {
+        this.project = project;
+    }
+}
