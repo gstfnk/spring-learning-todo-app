3a24b569373291eac619e7b9362ee0591725c96a
gstfnk <gabrysia.stefaniak@gmail.com>
2021-09-13 18:16:26 +0200

    add TestConfiguration

---
+++ src/test/java/io/github/gstfnk/TestConfiguration.java
@@ -0,0 +1,62 @@
+package io.github.gstfnk;
+
+import io.github.gstfnk.model.Task;
+import io.github.gstfnk.model.TaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+
+@Configuration
+class TestConfiguration {
+    @Bean
+    @Profile("integration")
+    TaskRepository testTaskRepository() {
+        return new TaskRepository() {
+            private final Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+
+            @Override
+            public Task save(Task entity) {
+                return tasks.put(tasks.size() + 1, entity);
+            }
+
+            @Override
+            public void deleteById(Integer id) {
+
+            }
+        };
+    }
+}
