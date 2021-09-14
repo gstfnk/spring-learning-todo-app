ffda90dc7acef016fcd7b465845b806a9b8cd8d0
gstfnk <gabrysia.stefaniak@gmail.com>
2021-09-14 21:28:05 +0200

    add mvcConfig

--- src/main/java/io/github/gstfnk/controller/MvcConfiguration.java
+++ src/main/java/io/github/gstfnk/controller/MvcConfiguration.java
@@ -1,2 +1,22 @@
-package io.github.gstfnk.controller;public class MvcConfiguration {
+package io.github.gstfnk.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Set;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    private final Set<HandlerInterceptor> interceptorSet;
+
+    MvcConfiguration(Set<HandlerInterceptor> interceptorSet) {
+        this.interceptorSet = interceptorSet;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        interceptorSet.forEach(registry::addInterceptor);
+    }
 }
