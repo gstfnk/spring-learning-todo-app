b0dddb1c155ec508192d4b8ba9ef9960ee702900
gstfnk <gabrysia.stefaniak@gmail.com>
2021-09-18 02:00:59 +0200

    improve GroupTask projection

--- src/main/java/io/github/gstfnk/model/projection/GroupWriteModel.java
+++ src/main/java/io/github/gstfnk/model/projection/GroupWriteModel.java
@@ -3,12 +3,21 @@ package io.github.gstfnk.model.projection;
 import io.github.gstfnk.model.Project;
 import io.github.gstfnk.model.TaskGroup;
 
-import java.util.Set;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class GroupWriteModel {
+    @NotBlank(message = "Task group's description must be not empty")
     private String description;
-    private Set<GroupTaskWriteModel> tasks;
+    @Valid
+    private List<GroupTaskWriteModel> tasks = new ArrayList<>();
+
+    public GroupWriteModel() {
+        tasks.add(new GroupTaskWriteModel());
+    }
 
     public String getDescription() {
         return description;
@@ -18,11 +27,11 @@ public class GroupWriteModel {
         this.description = description;
     }
 
-    public Set<GroupTaskWriteModel> getTasks() {
+    public List<GroupTaskWriteModel> getTasks() {
         return tasks;
     }
 
-    public void setTasks(Set<GroupTaskWriteModel> tasks) {
+    public void setTasks(List<GroupTaskWriteModel> tasks) {
         this.tasks = tasks;
     }
 
