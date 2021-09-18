1d7b99a9c0bb6aec8be66990ec485b69265db3a7
gstfnk <gabrysia.stefaniak@gmail.com>
2021-09-18 02:01:15 +0200

    add TaskGroupController

--- src/main/java/io/github/gstfnk/controller/TaskGroupController.java
+++ src/main/java/io/github/gstfnk/controller/TaskGroupController.java
@@ -4,12 +4,15 @@ import io.github.gstfnk.logic.TaskGroupService;
 import io.github.gstfnk.model.Task;
 import io.github.gstfnk.model.TaskRepository;
 import io.github.gstfnk.model.projection.GroupReadModel;
+import io.github.gstfnk.model.projection.GroupTaskWriteModel;
 import io.github.gstfnk.model.projection.GroupWriteModel;
-import org.hibernate.validator.internal.engine.groups.GroupWithInheritance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -17,7 +20,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/groups")
 class TaskGroupController {
     private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -29,22 +32,53 @@ class TaskGroupController {
         this.repository = repository;
     }
 
-    @GetMapping()
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<GroupReadModel>> readAllGroups() {
         return ResponseEntity.ok(service.readAll());
     }
 
-    @GetMapping(path = "/{id}")
+    @ResponseBody
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id) {
         return ResponseEntity.ok(repository.findAllByGroup_Id(id));
     }
 
-    @PostMapping
+    @ResponseBody
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) {
         GroupReadModel result = service.createGroup(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
+    @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
+    String addGroupTask(@ModelAttribute("group") GroupWriteModel current) {
+        current.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_HTML_VALUE)
+    String addGroup(@ModelAttribute("group") @Valid GroupWriteModel current,
+                    BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        service.createGroup(current);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Group has been added");
+        return "groups";
+    }
+
+    @ResponseBody
     @Transactional
     @PatchMapping(path = "/{id}")
     public ResponseEntity<?> toggleGroup(@PathVariable Integer id) {
@@ -60,5 +94,10 @@ class TaskGroupController {
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ModelAttribute("groups")
+    List<GroupReadModel> getGroups() {
+        return service.readAll();
     }
 }
