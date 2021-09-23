c52bb0586d6662cac11722f4a49ad5bb5417e3db
gstfnk <gabrysia.stefaniak@gmail.com>
2021-09-24 00:07:06 +0200

    separate ExceptionHandler to annotation

---
+++ src/main/java/io/github/gstfnk/controller/IllegalExceptionsControllerAdvice.java
@@ -0,0 +1,19 @@
+package io.github.gstfnk.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = IllegalExceptionProcessing.class)
+class IllegalExceptionsControllerAdvice {
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
--- src/main/java/io/github/gstfnk/controller/TaskGroupController.java
+++ src/main/java/io/github/gstfnk/controller/TaskGroupController.java
@@ -21,6 +21,7 @@ import java.net.URI;
 import java.util.List;
 
 @Controller
+@IllegalExceptionProcessing
 @RequestMapping(path = "/groups")
 class TaskGroupController {
     private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -84,16 +85,6 @@ class TaskGroupController {
     public ResponseEntity<?> toggleGroup(@PathVariable Integer id) {
         service.toggleGroup(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ModelAttribute("groups")
